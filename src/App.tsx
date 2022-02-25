16c581c178026715eb9ecf8c292c5562b5d0bd07
sansai-SNCT <syunzo1115@icloud.com>
2022-02-25 13:40:04 +0900

    [update] ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import { FC } from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Screen from './styles/screen';
 const App: FC = () => {
-  return <div>base</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Screen />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
