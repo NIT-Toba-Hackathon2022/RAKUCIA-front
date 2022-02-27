7370f10cc43a5b0b14de4b5ea4db7df9cc189231
sansai-SNCT <syunzo1115@icloud.com>
2022-02-27 14:28:11 +0900

    [update] 選択メニューを配置

--- src/styles/screen.tsx
+++ src/styles/screen.tsx
@@ -1,10 +1,22 @@
-import { Button } from '@mui/material';
-import { red } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import { FC } from 'react';
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 import './style.css';
 
+const Choosemenu = () => {
+  return (
+    <div>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox />} label="とんかつ" />
+        <FormControlLabel control={<Checkbox />} label="スープ" />
+      </FormGroup>
+    </div>
+  );
+};
 const Screen: FC = () => {
   const title = <h1 className="title">CRACIA</h1>;
   const text1 = <p className="text">作るものを選択してください</p>;
@@ -15,6 +27,7 @@ const Screen: FC = () => {
       {title}
       {text1}
       <hr className="line" />
+      <Choosemenu />
       {text2}
       <hr className="line" />
     </div>
