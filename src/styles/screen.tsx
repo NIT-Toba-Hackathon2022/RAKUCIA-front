64ac1cdcea2012408ff2d11b840c2973bd919df5
sansai-SNCT <syunzo1115@icloud.com>
2022-02-27 13:42:40 +0900

    [update] 境界線追加

--- src/styles/screen.tsx
+++ src/styles/screen.tsx
@@ -1,8 +1,21 @@
+import { Button } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import { FC } from 'react';
+import './style.css';
 
 const Screen: FC = () => {
-  return <div>home</div>;
+  const title = <h1 className="title">CRACIA</h1>;
+  const text1 = <h2>作るものを選択してください</h2>;
+  const text2 = <h2>材料リスト</h2>;
+
+  return (
+    <div>
+      {title}
+      <hr className="line" />
+    </div>
+  );
 };
 
 export default Screen;
