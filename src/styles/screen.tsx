aaae9d67685fac1ffec98666b592441c5e42daea
sansai-SNCT <syunzo1115@icloud.com>
2022-03-02 22:26:20 +0900

    [update] 汎用化の準備

--- src/styles/screen.tsx
+++ src/styles/screen.tsx
@@ -1,30 +1,34 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 import './style.css';
 
+//userが選択した料理をAPIから受け取り、リストで表示するコンポーネント
+
 const Choosemenu = () => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
   return (
     <div>
-      <FormGroup>
-        <FormControlLabel control={<Checkbox />} label="とんかつ" />
-        <FormControlLabel control={<Checkbox />} label="スープ" />
-      </FormGroup>
+      <Checkbox checked={checked} onChange={handleChange} />
     </div>
   );
 };
+
+//選択された料理の材料のデータを取得し表示するコンポーネント
+
+//UI構成
 const Screen: FC = () => {
-  const title = <h1 className="title">CRACIA</h1>;
   const text1 = <p className="text">作るものを選択してください</p>;
   const text2 = <p className="text">材料リスト</p>;
 
   return (
     <div>
-      {title}
       {text1}
       <hr className="line" />
       <Choosemenu />
