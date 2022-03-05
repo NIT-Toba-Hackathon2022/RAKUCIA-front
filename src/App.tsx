9583ad98e4aa55eef07ed2a31cc1685fc0960bd6
sansai-SNCT <syunzo1115@icloud.com>
2022-03-06 00:48:08 +0900

    [delete] 不要なファイルの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import liff from '@line/liff';
 import Screen from './styles/screen';
 const App: FC = () => {
+  useEffect(() => {
+    liff.ready.then(() => {
+      liff
+        .init({ liffId: process.env.REACT_APP_LIFF_ID as string })
+        .then(() => {
+          if (!liff.isLoggedIn()) {
+            liff.login();
+          }
+        });
+    });
+  }, []);
   return (
     <div>
       <Router>
