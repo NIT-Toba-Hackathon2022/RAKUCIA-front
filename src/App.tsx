ff67297afd96e04b560f6e2867cfc41a4faae6dc
Re-taro <aminokacho@gmail.com>
2022-03-07 01:05:53 +0900

    ✨ init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
-import * as React from 'react';
-import { FC, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import liff from '@line/liff';
-import Screen from './styles/screen';
-const App: FC = () => {
-  useEffect(() => {
-    liff.ready.then(() => {
-      liff
-        .init({ liffId: process.env.REACT_APP_LIFF_ID as string })
-        .then(() => {
-          if (!liff.isLoggedIn()) {
-            liff.login();
-          }
-        });
-    });
-  }, []);
-  return (
-    <div>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route element={<Screen />} />
-          <Route path="/" element={<Screen />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-export default App;
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
