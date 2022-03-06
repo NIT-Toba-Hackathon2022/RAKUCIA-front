294e6acbcf5c3e228fb6d8b385becdbcb5b61076
Bayathy <misosoup0321@icloud.com>
2022-03-06 23:13:07 +0900

    Good Night! Sansai-SNCT

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,6 +1,4 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import liff from '@line/liff';
 import gql from 'graphql-tag';
 
